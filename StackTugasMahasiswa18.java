a6746822e2a1eaddc6f6ef13ada5198a2af33179
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2026-04-21 12:45:31 +0700

    class stack

--- StackTugasMahasiswa18.java
+++ StackTugasMahasiswa18.java
@@ -45,6 +45,15 @@ public class StackTugasMahasiswa18 {
         }
     }
 
+    public Mahasiswa18 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
     public void print() {
         for (int i = 0 ; i <= top ; i++) {
             System.out.println(stack[i].nama + "\t" +stack[i].nim + "\t" + stack[i].kelas);
