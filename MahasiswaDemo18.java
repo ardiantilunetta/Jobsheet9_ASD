327941e9464dc6e64fd5c2291605b629c9ad4047
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2026-04-21 11:59:50 +0700

    class stack

---
+++ MahasiswaDemo18.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MahasiswaDemo18 {
+    public static void main(String[] args) {
+        StackTugasMahasiswa18 stack = new StackTugasMahasiswa18(5);
+        Scanner scan = new Scanner(System.in);
+        int pilih;
+    }
+}
